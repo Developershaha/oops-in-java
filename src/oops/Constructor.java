72971a9270b64afde7ec6e4646b3c85480c730bd
Developershaha <shahajikokane9049@gmail.com>
2025-10-12 22:54:19 +0530

    first commit

---
+++ src/oops/Constructor.java
@@ -0,0 +1,20 @@
+package oops;
+
+public class Constructor {
+
+    // rules to define constructor
+/*
+    1) name should be as clas name
+    2) not having any return type even void is not acceptable
+    3) there are multiple java constructor means java constructor follows the overloding but not follow overriding
+    4) we can applied the access modified to the constructor
+    5) java called default constructor at the time of object creation
+
+
+
+    */
+
+
+
+
+}

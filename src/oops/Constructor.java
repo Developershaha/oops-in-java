24cb65625547554ed7224dc72025247aecf8a6b6
Developershaha <shahajikokane9049@gmail.com>
2025-10-25 08:22:39 +0530

    added construction code

--- src/oops/Constructor.java
+++ src/oops/Constructor.java
@@ -22,14 +22,34 @@ public Constructor(
 
     */
 
-    public Constructor(){
+    int num1;
+    int num2;
 
+    public Constructor() {
+//        this()
+        num2 = 44;
         System.out.println("calling no args constructor ");
+    }
+
+    Constructor(int num) {
+//        this(4);
+        this.num1 = num;
+    }
+
+    Constructor(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    void display() {
+
+        System.out.println("print num1 and num2 " + num1 + ",  " + num2);
     }
 
 
     public static void main(String[] args) {
-        Constructor c=new Constructor();
+        Constructor c = new Constructor(4);
+        c.display();
     }
 
 }
